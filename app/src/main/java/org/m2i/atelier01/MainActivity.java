15e5d789297e4eea45b903122d7df9fc765e7546
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-05-03 13:04:31 +0100

    final commit

--- app/src/main/java/org/m2i/atelier01/MainActivity.java
+++ app/src/main/java/org/m2i/atelier01/MainActivity.java
@@ -6,20 +6,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button login_btn ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("testtest","onCreate #####");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d("testtest","onStart #####");
+        login_btn = findViewById(R.id.btn_logIN);
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), NextActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -46,8 +55,5 @@ public class MainActivity extends AppCompatActivity {
         Log.d("testtest","onDestroy #####");
     }
 
-    public void login(View view) {
-        Intent intent = new Intent(this, NextActivity.class);
-        startActivity(intent);
-    }
+
 }
